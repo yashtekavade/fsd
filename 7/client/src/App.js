ad2561c644eca9aadf1c8510930c5bcc11370b4c
bishh <yashtekavade@gmail.com>
2023-12-04 01:25:43 +0530

    .

--- 7/client/src/App.js
+++ 7/client/src/App.js
@@ -1,56 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+import React from "react";
+import InsertEmployeeForm from ".../InsertEmployeeForm.js";
+import UpdateEmployeeForm from "./UpdateEmployeeForm.js";
+import DeleteEmployeeForm from "./DeleteEmployeeForm.js";
+import DisplayEmployeeRecords from "./DisplayEmployeeRecords.js";
 
-const app = express();
-const PORT = process.env.PORT || 5000; // Update the port if needed
+function App() {
+  return (
+    <div className="App">
+      <h1>Employee Management System</h1>
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+      {/* Insert Employee Form */}
+      <InsertEmployeeForm />
 
-// Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://1234:<password>@cluster0.oahhxv2.mongodb.net/?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+      {/* Update Employee Form */}
+      <UpdateEmployeeForm />
 
-// Create Mongoose model
-const Employee = mongoose.model("Employee", {
-  employeeName: String,
-  employeeID: String,
-  departmentName: String,
-  phoneNumber: String,
-  joiningDate: Date,
-});
+      {/* Delete Employee Form */}
+      <DeleteEmployeeForm />
 
-// Set up routes
-app.post("/saveData", (req, res) => {
-  const { employeeName, employeeID, departmentName, phoneNumber, joiningDate } =
-    req.body;
+      {/* Display Employee Records */}
+      <DisplayEmployeeRecords />
+    </div>
+  );
+}
 
-  // Create a new Employee instance
-  const newEmployee = new Employee({
-    employeeName,
-    employeeID,
-    departmentName,
-    phoneNumber,
-    joiningDate,
-  });
-
-  // Save to MongoDB
-  newEmployee.save((err) => {
-    if (err) {
-      res.status(500).send(err.message);
-    } else {
-      res.status(200).send("Employee data saved successfully!");
-    }
-  });
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default App;
